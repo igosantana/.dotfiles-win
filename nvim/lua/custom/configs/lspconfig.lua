299523b124b71b89b0bcf8e40b902a55144bacc4
Igo Santana <igo.santana33@gmail.com>
2023-06-14 16:31:46 -0300

    feat new

--- nvim/lua/custom/configs/lspconfig.lua
+++ nvim/lua/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "pyright", "csharp_ls" }
+local servers = { "html", "cssls", "tsserver", "clangd", "pyright", "csharp_ls", "angularls" }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
